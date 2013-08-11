64822fbe973e56bb41d2a9e1e4fe7254fb45296f
Christian Sumido <gcbsumid@gmail.com>
2013-08-11 01:30:09 -0400

    Turns out I forgot to commit defines.h ~ haha my bad.

---
+++ src/Util/defines.h
@@ -0,0 +1,14 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+// Glm Library
+#include <glm/glm.hpp>
+
+namespace utility {
+    const glm::vec2 SCREEN_SIZE(800, 600);
+    const glm::vec3 DEFAULT_POSITION(0,0,4);
+    const float     DEFAULT_NEAR_PLANE = 0.5f;
+    const float     DEFAULT_FAR_PLANE = 100.0f;
+}
+
+#endif
